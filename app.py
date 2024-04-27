753453c1b9ef90eb5d4cdbb629311fb1cb0e6a6a
Daniel Plotnikov <daniilrew@gmail.com>
2024-04-27 19:41:07 +0500

    webui 1.0.2704

---
+++ app.py
@@ -0,0 +1,132 @@
+# 
+# Import libs
+# 
+import dash
+from dash import html, Output, Input, State, callback, dcc, clientside_callback
+import dash_mantine_components as dmc
+import dash_bootstrap_components as dbc
+
+# 
+# app config
+# 
+app = dash.Dash(
+    __name__,
+    use_pages=True,
+    external_stylesheets=[dbc.themes.ZEPHYR, dbc.icons.FONT_AWESOME],
+    title="Dash Messenger",
+    update_title="Dash Messenger 🔄️",
+    external_scripts=["/assets/size.js"],
+)
+
+# 
+# static layout
+# 
+header = dbc.Navbar(
+    dbc.Container(
+        [
+            html.A(
+                # Use row and col to control vertical alignment of logo / brand
+                dbc.Row(
+                    [
+                        # dbc.Col(html.Img(src=PLOTLY_LOGO, height="30px")),
+                        dbc.Col(dbc.NavbarBrand("Dash Messenger", className="ms-2")),
+                    ],
+                    align="center",
+                    className="g-0",
+                ),
+                href="/",
+                style={"textDecoration": "none"},
+            ),
+            dbc.NavbarToggler(id="navbar-toggler", n_clicks=0),
+            dbc.Collapse(
+                dbc.Row(
+                    [
+                        # dbc.Col(dbc.Input(type="search", placeholder="Search")),
+                        dbc.Col(
+                            html.Span(
+                                [
+                                    dbc.Label(className="fa fa-moon", html_for="color-mode-switch", color='secondary'),
+                                    dbc.Switch(id="color-mode-switch", value=True, className="d-inline-block ms-1", persistence=True),
+                                    dbc.Label(className="fa fa-sun", html_for="color-mode-switch", color='secondary'), 
+                                ]
+                            ),
+                            width="auto",
+                        ),
+                    ],
+                    className="g-0 ms-auto flex-nowrap mt-3 mt-md-0",
+                    align="center",
+                ),
+                id="navbar-collapse",
+                is_open=False,
+                navbar=True,
+            ),
+        ]
+    ),
+    color="dark",
+    dark=True,
+    style={'width': '100%'},
+    className='border-bottom'
+)
+
+main_container = dmc.Grid(
+    [
+        dmc.Col(span=1, className='adaptive-hide'),
+        # dmc.Col([dash.page_container], span=10, className='border border-top-0 adaptive-width', h='100%'),
+        dmc.Col(dash.page_container, span=10, className='adaptive-width child-height-100', h='100%'),
+        dmc.Col(span=1, className='adaptive-hide'),
+    ],
+    align='center',
+    className='adaptive-width',
+    h='100%',
+    p='0',
+)
+
+
+app.layout = dmc.Container(
+    [
+        header,
+        main_container
+    ],
+    miw='100%',
+    maw='100%',
+    p='0',
+    className='full-height'
+    # style={'minWidth': '100% !important'}
+)
+
+# add callback for toggling the collapse on small screens
+@app.callback(
+    Output("navbar-collapse", "is_open"),
+    [Input("navbar-toggler", "n_clicks")],
+    [State("navbar-collapse", "is_open")],
+)
+def toggle_navbar_collapse(n, is_open):
+    if n:
+        return not is_open
+    return is_open
+
+clientside_callback(
+    """
+    (switchOn) => {
+       document.documentElement.setAttribute('data-bs-theme', switchOn ? 'light' : 'dark');  
+       return window.dash_clientside.no_update
+    }
+    """,
+    Output("color-mode-switch", "id"),
+    Input("color-mode-switch", "value"),
+)
+
+#
+# Dash server launcher
+#
+server = app.server
+app.config.suppress_callback_exceptions = True
+
+dev = True
+
+if __name__ == "__main__":
+    if dev: 
+        app.run_server(debug=True, host="0.0.0.0", port=82)
+    else: 
+        from waitress import serve
+        serve(app.server, host="0.0.0.0", port=82)
---
+++ pages/home.py
@@ -0,0 +1,90 @@
+"Домашняя страница."
+"Если пользователь не авторизован - показываем форму авторизации"
+"Если пользователь авторизован - перенаправляем его на страницу '/messenger/im'"
+
+from dash import (
+    dcc,
+    html,
+    Input,
+    Output,
+    callback,
+    register_page,
+    State,
+    Input,
+    Output,
+    no_update,
+    ALL,
+)
+import dash_mantine_components as dmc
+import dash_bootstrap_components as dbc
+from dash_iconify import DashIconify
+
+register_page(__name__, path="/", icon="fa-solid:home", name="Главная | Dash Messenger")
+
+
+def layout():
+    email_input = html.Div(
+        [
+            dbc.Label("Электронная почта", html_for="example-email"),
+            dbc.Input(type="email", id="example-email", placeholder="Введите email"),
+        ],
+        className="mb-3",
+    )
+
+    password_input = html.Div(
+        [
+            dbc.Label("Пароль", html_for="example-password"),
+            dbc.Input(
+                type="password",
+                id="example-password",
+                placeholder="Введите пароль",
+            ),
+        ],
+        className="mb-3",
+    )
+
+    return dmc.Stack(
+        [
+            dmc.Grid(
+                [
+                    dmc.Col(span=6, className='adaptive-hide'),
+                    dmc.Col([
+                        dmc.Stack(
+                            [
+                                dbc.Card(
+                                    [
+                                        html.H4("Dash Messenger", className='text-center'),
+                                        html.H5("Основан на технологии WebSocket", className='text-center'),
+                                        dmc.Space(h=10),
+                                        dbc.Form([
+                                            email_input, 
+                                            password_input,
+                                            dbc.Button("Войти в аккаунт", style={'width': '100%'})
+                                        ], style={'width': '65%', 'margin': 'auto'}, class_name='adaptive-input-width'),
+                                    ],
+                                    style={'width': '100%', 'padding': '20px'}
+                                ),
+                                dbc.Card(
+                                    dmc.Stack(
+                                        [
+                                            html.H5("Нет аккаунта или забыли пароль?", className='text-center'),
+                                            dbc.Button("Создать аккаунт", style={'width': 'content'}),
+                                            dbc.Button("Восстановить пароль", style={'width': 'content'}),
+                                        ],
+                                        align='center'
+                                    ),
+                                    style={'width': '100%', 'padding': '20px'}
+                                ),
+                            ]
+                        )
+                    ], span=6, className='adaptive-width'),
+                ],
+                style={'width': '100%'}
+            ),
+        ],
+        align="center",
+        # justify="center",
+        h="100%",
+        pt='10vh',
+        className="child-autoheight",
+    )
