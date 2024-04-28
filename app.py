a01395c42824e0b59cef6ed669c82c29ac78eb4a
Daniel Plotnikov <daniilrew@gmail.com>
2024-04-28 22:25:52 +0500

    webui 1.3.2804

--- app.py
+++ app.py
@@ -22,8 +22,8 @@ temp_buttons = dmc.Group([
     html.A(
         "Мессенджер",
         style={"width": "content"},
-        className='btn btn-primary disabled btn-sm',
-        href='/messenger/im',
+        className='btn btn-primary btn-sm',
+        href='/im',
     ),
     html.A(
         "Управление аккаунтом",
@@ -80,20 +80,20 @@ header = dbc.Navbar(
     color="dark",
     dark=True,
     style={'width': '100%'},
-    className='border-bottom roww fit-content'
+    className='border-bottom fit-content roww'
 )
 
 main_container = dmc.Grid(
     [
         dmc.Col(span=1, className='adaptive-hide'),
         # dmc.Col([dash.page_container], span=10, className='border border-top-0 adaptive-width', h='100%'),
-        dmc.Col(dash.page_container, span=10, className='adaptive-width child-height-100', h='100%'),
+        dmc.Col(dash.page_container, span=10, className='adaptive-width child-height-100', mah='100%'),
         dmc.Col(span=1, className='adaptive-hide'),
     ],
     align='center',
-    className='roww fill-remain',
+    className='adaptive-width fill-remain roww',
     # className='adaptive-width',
-    h='100%',
+    mah='100%',
     p='0',
 )
 
